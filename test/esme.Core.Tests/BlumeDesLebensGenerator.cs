9d6814829475763e3e7a68e791a7baa71bcff75c
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-05-03 23:31:36 +0200

    draw all arcs

--- test/esme.Core.Tests/BlumeDesLebensGenerator.cs
+++ test/esme.Core.Tests/BlumeDesLebensGenerator.cs
@@ -7,6 +7,8 @@ namespace esme.Core.Tests
     [TestFixture]
     public class BlumeDesLebensGenerator
     {
+        private const double SixtyDegInRad = Math.PI / 3.0;
+
         [Test] // FIXME: da, change to Explicit
         public void Generate()
         {
@@ -21,11 +23,24 @@ namespace esme.Core.Tests
 
             foreach (var (x, y) in ComputeCenters(Center, Center, InnerRadius))
             {
-                double distanceToCenter = Math.Sqrt(Math.Pow(Center - x, 2) + Math.Pow(Center - y, 2));
-                bool overlapsOuterCircle = distanceToCenter > OuterRadius - InnerRadius + 1.0;
+                double distanceToCenter = Distance((Center, Center), (x, y));
+                bool overlapsOuterCircle = IsGreaterThan(distanceToCenter, OuterRadius - InnerRadius);
                 if (overlapsOuterCircle)
                 {
-
+                    for (int i = 0; i < 6; i++)
+                    {
+                        double startAngle = i * SixtyDegInRad;
+                        double endAngle = (i + 1) * SixtyDegInRad;
+                        var start = PolarToCartesian(x, y, InnerRadius, endAngle);
+                        var end = PolarToCartesian(x, y, InnerRadius, startAngle);
+                        bool arcCrossesOuterCircle =
+                            IsGreaterThan(Distance((Center, Center), start), OuterRadius) ||
+                            IsGreaterThan(Distance((Center, Center), end), OuterRadius);
+                        if (!arcCrossesOuterCircle)
+                        {
+                            svgElements.Add(DrawArc(x, y, InnerRadius, startAngle, endAngle));
+                        }
+                    }
                 }
                 else
                 {
@@ -38,15 +53,24 @@ namespace esme.Core.Tests
             }
         }
 
+        private static bool IsGreaterThan(double a, double b)
+        {
+            return a > b + 1.0;
+        }
+
+        private static double Distance((double x, double y) p1, (double x, double y) p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+        }
+
         private IEnumerable<(double, double)> ComputeCenters(double X, double Y, double R, int level = 0)
         {
-            if (level > 2) yield break;
+            if (level > 3) yield break;
 
-            const double sixtyDegInRad = Math.PI / 3.0;
             for (int i = 0; i < 6; i++)
             {
-                double x = X + Math.Sin(i * sixtyDegInRad) * R;
-                double y = Y + Math.Cos(i * sixtyDegInRad) * R;
+                double x = X + Math.Cos(i * SixtyDegInRad) * R;
+                double y = Y + Math.Sin(i * SixtyDegInRad) * R;
                 yield return (x, y);
                 foreach (var center in ComputeCenters(x, y, R, level + 1))
                 {
@@ -67,7 +91,7 @@ namespace esme.Core.Tests
 
             string largeArcFlag = endAngle - startAngle <= Math.PI ? "0" : "1";
 
-            var d = $"M {start.x} {start.y} A {radius} {radius} 0 {largeArcFlag} 0 {end.x} {end.y}";
+            var d = $"M {start.x:0.####} {start.y:0.####} A {radius} {radius} 0 {largeArcFlag} 0 {end.x:0.####} {end.y:0.####}";
 
             return $"            <path d=\"{d}\" stroke-width=\"1\" fill=\"none\" />";
         }

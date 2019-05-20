7ffc621fea4e1a4f8d758b44abe4a57a5e0e8bcf
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-05-20 21:59:46 +0200

    added first migration

--- src/esme.Server/Program.cs
+++ src/esme.Server/Program.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore;
+﻿using esme.Infrastructure.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace esme.Server
 {
@@ -8,7 +13,32 @@ namespace esme.Server
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger("Migrations");
+                logger.LogInformation("Start upgrading database.");
+                try
+                {
+                    // apply all outstanding migrations:
+                    var db = services.GetService<ApplicationDbContext>();
+                    db.Database.SetCommandTimeout(600);
+                    db.Database.Migrate();
+                    db.Database.SetCommandTimeout(30);
+                }
+                catch (Exception x)
+                {
+                    logger.LogError($"Error occured during migrations: {x.Message}, {x.InnerException} {x.InnerException?.Message}");
+                    throw;
+                }
+                finally
+                {
+                    logger.LogInformation("Finished updgrading database.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>

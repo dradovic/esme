b16cea99ee9da7163444f69e3040a20355991bef
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-06-06 00:34:46 +0200

    seed one message in Open Circle

--- src/esme.Admin.Infrastructure/Services/SampleDataService.cs
+++ src/esme.Admin.Infrastructure/Services/SampleDataService.cs
@@ -12,14 +12,40 @@ namespace esme.Admin.Infrastructure.Services
     {
         private readonly SampleDataOptions _options;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
 
-        public SampleDataService(IOptions<SampleDataOptions> options, UserManager<ApplicationUser> userManager)
+        public SampleDataService(IOptions<SampleDataOptions> options, UserManager<ApplicationUser> userManager,
+            ApplicationDbContext db)
         {
             _options = options.Value;
             _userManager = userManager;
+            _db = db;
         }
 
         public async Task ResetAllWithSampleData()
+        {
+            await DeleteAll();
+            await AddUser();
+            AddData();
+            await _db.SaveChangesAsync();
+        }
+
+        private void AddData()
+        {
+            //_db.Circles.Add(new Circle
+            //{
+            //    Name = "My First Circle",
+            //});
+            _db.Messages.Add(new Message
+            {
+                CircleId = Circle.OpenCircle.Id,
+                Text = "Hello everybody!",
+                SentAt = DateTimeOffset.UtcNow,
+                SentBy = _options.UserName,
+            });
+        }
+
+        private async Task AddUser()
         {
             var user = new ApplicationUser
             {
@@ -31,6 +57,32 @@ namespace esme.Admin.Infrastructure.Services
             {
                 string errors = string.Join(", ", result.Errors.Select(e => e.Description));
                 throw new InvalidOperationException($"Could not create sample user: {errors}.");
+            }
+        }
+
+        private async Task DeleteAll()
+        {
+            await DeleteAllUsers();
+            DeleteAllData();
+        }
+
+        private void DeleteAllData()
+        {
+            foreach (var circle in _db.Circles.Where(c => c.Id >= 0))
+            {
+                _db.Circles.Remove(circle);
+            }
+            foreach (var message in _db.Messages)
+            {
+                _db.Messages.Remove(message);
+            }
+        }
+
+        private async Task DeleteAllUsers()
+        {
+            foreach (var user in _db.Users.ToArray())
+            {
+                await _userManager.DeleteAsync(user);
             }
         }
     }

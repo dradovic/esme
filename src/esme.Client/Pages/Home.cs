d8a28090de4faddea54b03e1c2b234e5e2c344b2
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-06-05 11:30:38 +0200

    Home: split code-behind

---
+++ src/esme.Client/Pages/Home.cs
@@ -0,0 +1,20 @@
+﻿using esme.Shared.Circles;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace esme.Client.Pages
+{
+    public abstract class HomeBase : ComponentBase
+    {
+        protected CircleViewModel[] _circles;
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            _circles = await Http.GetJsonAsync<CircleViewModel[]>("api/my/circles");
+        }
+    }
+}

53fa9182cc2498778a8ae140dce3dafcf9f03283
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-07-01 06:33:27 +0200

    styled balloons

--- src/esme.Client/Pages/Balloon.razor.cs
+++ src/esme.Client/Pages/Balloon.razor.cs
@@ -1,5 +1,7 @@
 ﻿using esme.Shared.Circles;
 using Microsoft.AspNetCore.Components;
+using esme.Client.Services;
+using AuthenticationState = esme.Client.Services.AuthenticationState; // FIXME: da, eliminate ours and use the provided one
 
 namespace esme.Client.Pages
 {
@@ -7,5 +9,16 @@ namespace esme.Client.Pages
     {
         [Parameter]
         protected MessageViewModel Message { get; set; }
+
+        [Inject]
+        private AuthenticationState AuthenticationState { get; set; }
+
+        protected bool IsMine
+        {
+            get
+            {
+                return Message.SenderName == AuthenticationState.User.UserName; // FIXME: da, rather check eq of Id
+            }
+        }
     }
 }

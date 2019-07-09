991d7e4b90caa70e531c3114cc2f728092f454f6
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-07-09 08:46:06 +0200

    introduce main container CSS class

---
+++ src/esme.Client/Shared/Classes.cs
@@ -0,0 +1,7 @@
+﻿namespace esme.Client.Shared
+{
+    public static class Classes
+    {
+        public const string MainContainer = "container text-center col-md-6 offset-md-3 col-lg-4 offset-lg-4";
+    }
+}

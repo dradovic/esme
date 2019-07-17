f897a7cff95b04d81eaae3059cf8497fc7355838
Dejan Appenzeller <dejan.appenzeller@gmail.com>
2019-07-17 22:19:39 +0200

    fixed deserialization issue

--- src/esme.Client/Services/HttpClientExtensions.cs
+++ src/esme.Client/Services/HttpClientExtensions.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json.Serialization;
 using System;
+using Newtonsoft.Json;
 
 namespace esme.Client.Services
 {
@@ -11,12 +12,14 @@ namespace esme.Client.Services
         // FIXME: da, use this method everywhere
         public static async Task<T> PostAsync<T>(this HttpClient httpClient, string requestUri, object payload, Action<string> onError)
         {
-            var payloadContent = new StringContent(JsonSerializer.ToString(payload), Encoding.UTF8, "application/json");
+            var payloadContent = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"); // FIXME: da, remove ref. to Newtonsoft and use JsonSerializer
+            //var payloadContent = new StringContent(JsonSerializer.ToString(payload), Encoding.UTF8, "application/json");
             var response = await httpClient.PostAsync(requestUri, payloadContent);
             var responseContent = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
-                return JsonSerializer.Parse<T>(responseContent);
+                return JsonConvert.DeserializeObject<T>(responseContent);
+                //return JsonSerializer.Parse<T>(responseContent);
             }
             else
             {
